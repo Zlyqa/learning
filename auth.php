778ee5a302b004b57cbdd78109a94dd4b920ec39
Zlyqa <fzprog1@gmail.com>
2024-10-12 10:42:10 +0300

    up

--- auth.php
+++ auth.php
@@ -1,2 +1,23 @@
-<?php
-$a = 1;
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+<h3>Атворизация</h3>
+<body>
+    <form action="index.php" method="post">
+        <label>Логин:<br>
+            <input type="text" name="login" required><br>
+        </label>
+        <label>Пароль:<br>
+            <input type="password" name="password" required><br>
+        </label>
+        <input type="submit" value="Войти">
+    </form>
+    <p>
+        <a href="register.php">Зарегистрироваться</a>
+    </p>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $login = htmlspecialchars($_POST["login"], ENT_QUOTES);
+    $password = htmlspecialchars($_POST["password"], ENT_QUOTES);
+    if (array_key_exists("password2", $_POST)) {
+        $email = htmlspecialchars($_POST["Email"], ENT_QUOTES);
+        $password2 = htmlspecialchars($_POST["password2"], ENT_QUOTES);
+        if ($password2 == $password) {
+            echo "Регистрация прошла успешно <br>Почта: $email <br>Логин: $login <br>Пароль: $password";
+        } else {
+            echo "Пароли не совпадают";
+        }
+    } else {
+        echo "Пользователь авторизован <br>Логин: $login <br>Пароль: $password";
+    }
+}
